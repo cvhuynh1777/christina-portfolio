c85399d128c4244e3877cb6ce6470d964ffecfc2
christinah17 <christina.v.huynh1@gmail.com>
2026-02-02 23:29:32 -0800

    added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,20 @@ import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 
 export default function App() {
-  const [windows, setWindows] = useState([]);
-
-  // Spawn a new draggable retro window
-  const openWindow = (component) => {
-    setWindows((prev) => [
-      ...prev,
-      { id: Date.now(), content: component }
-    ]);
-  };
-
   return (
     <div className="relative min-h-screen bg-pink-200">
 
-      {/* === Desktop Icons Layer === */}
-      {/*<div className="grid grid-cols-1 gap-6 absolute top-6 left-6 z-10">
-        <DesktopIcon label="Resume" onClick={() => openWindow(<Resume />)} />
-        <DesktopIcon label="Projects" onClick={() => openWindow(<Projects />)} />
-        <DesktopIcon label="About Me" onClick={() => openWindow(<Header />)} />
-        <DesktopIcon label="Contact" onClick={() => openWindow(<Contact />)} />
-      </div> */}
-
-      {/* === Main Window (ALWAYS OPEN) === */}
-      <PixelBrowser>
+      {/* Header + Resume + Contact (clean on mobile) */}
+      <PixelBrowser hideFrameOnMobile>
         <Header />
         <Resume />
-        <Projects />
         <Contact />
       </PixelBrowser>
 
-      {/* === Additional Spawned Windows === */}
-      {windows.map((w) => (
-        <PixelBrowser key={w.id}>
-          {w.content}
-        </PixelBrowser>
-      ))}
+      {/* Projects (always framed) */}
+      <PixelBrowser>
+        <Projects />
+      </PixelBrowser>
 
     </div>
   );
